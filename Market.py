0ab2a9c624fb5d021a51755020d871ddf1fa7684
Brendan Stimola <brendanstimola@gmail.com>
2026-08-23 15:30:16 -0400

    Add total_sales tracking to Market class
    Initialize total_sales and reset transactions in run method.

--- Market.py
+++ Market.py
@@ -1,16 +1,24 @@
-class Market: 
+class Market:
     def __init__(self):
-
         self.transactions = 0
+        self.total_sales = 0.0
 
     def run(self, households, firms):
+        self.transactions = 0
+        self.total_sales = 0.0
 
         for firm in firms:
-
-            firm.sales = 0
+            firm.reset_daily_financials()
 
         for household in households:
+            before_consumption = household.consumption
 
+            household.calculate_total_income()
             household.buy(firms)
 
-            self.transactions += 1
+            if household.consumption > before_consumption:
+                self.transactions += 1
+                self.total_sales += (
+                    household.consumption
+                    - before_consumption
+                )
