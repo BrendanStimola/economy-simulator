16f041858a91963b756eef6c900e86ac324b3260
Brendan Stimola <brendanstimola@gmail.com>
2026-08-23 15:27:04 -0400

    Refactor Household class financial attributes
    Refactor Household class to use float for financial attributes and improve income calculation methods.

--- Household.py
+++ Household.py
@@ -1,73 +1,81 @@
 import random
 
+
 class Household:
     def __init__(self, name):
-
-        self.money = 100
-
-        self.goods = 0
-
-        self.deposit = 0
-
-        self.loan = 0
-
-        self.wage_income = 0
-
-        self.transfer_income = 0
-
-        self.interest_income = 0
-
-        self.total_income = 0
-
-        self.consumption = 0
-
-        self.savings = 0
-
         self.name = name
 
-    def reset_daily_income(self):
-        self.wage_income = 0
-        self.transfer_income = 0
-        self.interest_income = 0
-        self.total_income = 0
-        self.consumption = 0
+        # Financial assets
+        self.money = 100.0
+        self.deposit = 0.0
+
+        # Debt
+        self.loan = 0.0
+
+        # Physical goods
         self.goods = 0
 
+        # Income
+        self.wage_income = 0.0
+        self.transfer_income = 0.0
+        self.interest_income = 0.0
+        self.total_income = 0.0
+
+        # Spending
+        self.consumption = 0.0
+
+    def reset_daily_income(self):
+        self.wage_income = 0.0
+        self.transfer_income = 0.0
+        self.interest_income = 0.0
+        self.total_income = 0.0
+        self.consumption = 0.0
+        self.goods = 0
+
+    def calculate_total_income(self):
+        self.total_income = (
+            self.wage_income
+            + self.transfer_income
+            + self.interest_income
+        )
+        return self.total_income
+
+    def financial_wealth(self):
+        return self.money + self.deposit
+
     def buy(self, firms):
+        spending_limit = self.total_income * 0.80
 
-        while self.consumption < self.total_income * 0.8 and self.money > 0:
+        while (
+            self.consumption < spending_limit
+            and self.money > 0
+        ):
+            affordable = [
+                firm
+                for firm in firms
+                if self.money >= firm.price and firm.inventory > 0
+            ]
 
-            affordable = []
-
-            for firm in firms:
-
-                if self.money >= firm.price and firm.inventory > 0:
-
-                    affordable.append(firm)
-
-
-            if len(affordable) > 0:
-
-                cheapest = min(affordable, key=lambda firm: firm.price)
-
-                self.money -= cheapest.price
-
-                self.consumption += cheapest.price
-
-                self.goods += 1
-
-                cheapest.money += cheapest.price
-
-                cheapest.inventory -= 1
-
-                cheapest.sales += 1
-
-            else:
-                
+            if not affordable:
                 break
+
+            cheapest = min(
+                affordable,
+                key=lambda firm: firm.price
+            )
+
+            self.money -= cheapest.price
+            cheapest.money += cheapest.price
+
+            cheapest.inventory -= 1
+            cheapest.sales += 1
+            cheapest.revenue += cheapest.price
+
+            self.consumption += cheapest.price
+            self.goods += 1
 
     def wants_to_withdraw(self):
         if self.deposit > 20:
-            if random.random() < 0.5:
-                return True
+            return random.random() < 0.5
+
         return False
